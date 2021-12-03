f1c1b3f3d81fe50f65ca601a147635cae4b004d7
AbdulmajeedS <abdulmajeed.sabriye777@gmai.com>
2021-12-03 01:09:14 +0000

    api

---
+++ w2/d4-api/breedFetcher.js
@@ -0,0 +1,32 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+
+  // checks for error
+  if (error) {
+    callback(error, null);
+    return;
+  }
+
+  const data = JSON.parse(body)[0];
+
+  // checks if breed exists
+
+  if (!data) {
+    callback('Breed not found!', null);
+    return;
+  }
+
+  // if there is a breed
+  if (data) {
+    callback(null, data.description);
+  } 
+
+
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
